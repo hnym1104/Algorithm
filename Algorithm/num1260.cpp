1ff41287930c33887674e9ca82fd6f0eb863e6a5
Yerim Hyun <gdf4013@naver.com>
2022-05-05 16:30:41 +0900

    백준 1260 DFS와 BFS sol

--- Algorithm/num1260.cpp
+++ Algorithm/num1260.cpp
@@ -1,79 +1,95 @@
-//for문 수정 vertex 크기만큼
-//DFS와 BFS
-#include <iostream>
-#include <algorithm>
 #include <vector>
-#include <stack>
 #include <queue>
+#include <iostream>
 using namespace std;
+
+/*
+4 5 1
+1 2
+1 3
+1 4
+2 4
+3 4
+
+1 2 4 3
+1 2 3 4
+==========
+5 5 3
+5 4
+5 2
+1 2
+3 4
+3 1
+
+3 1 2 5 4
+3 1 4 2 5
+==========
+1000 1 1000
+999 1000
+
+1000 999
+1000 999
+*/
+
+vector<vector<int>> graph;
+vector<bool> visited;
+
+void dfs(int x) {
+	visited[x] = true;
+	cout << x << " ";
+	for (int i = 0; i < graph[x].size(); i++) {
+		if (graph[x][i] == 1) {
+			if (!visited[i])
+				dfs(i);
+		}
+	}
+}
+
+void bfs(int x) {
+	queue<int> q;
+	visited[x] = true;
+	q.push(x);   // 초기 원소
+	while (!q.empty()) {
+		x = q.front();
+		cout << q.front() << " ";
+		for (int i = 0; i < graph[x].size(); i++) {
+			if (graph[x][i] == 1) {
+				if (!visited[i]) {
+					q.push(i);   // 인접한 노드 push
+					visited[i] = true;
+				}
+			}
+		}
+		q.pop();
+	}
+}
 
 int main(void)
 {
 	int vertex, edge, start;
-	int f, t, count = 0;
-	vector<bool> visited(1001, false);
-	vector<vector <int>> graph(1001, vector<int>(1001, 0));   // 가로 1000, 세로 1000(최대 vertex)
-	stack<int> D;
-	queue<int> B;
-
+	int p1, p2;
 	cin >> vertex >> edge >> start;
 
-	for (int i = 0; i < edge; i++)
-	{
-		cin >> f >> t;   // 연결할 두 정점 입력
-		graph[f][t] = 1;   // 인접행렬 
-		graph[t][f] = 1;
-	}
-
-	//dfs(stack 사용)
-	D.push(start);   // 시작 vertex stack에 넣음
-	visited[start] = true;   // 시작 정점은 방문했으므로 true;
-	cout << start << " ";
-
-	while (!D.empty())
-	{
-		count = 0;
-		int current = D.top();
-		visited[current] = true;
-		for (int i = 0; i < 1000; i++)
-		{
-			if (graph[current][i] == 1 && !visited[i])   // 연결되어있으면서 방문하지않은 노드
-			{
-				D.push(i);
-				cout << i << " ";
-				visited[i] = true;
-				count = 1;
-				break;
-			}
-		}
-		if (count == 0)   // 더 이상 방문 가능한 인접 노드가 없을 때
-			D.pop();
-	}
-
-	cout << endl;
+	graph.assign(1001, vector<int>(1001, 0));
 	visited.assign(1001, false);
-
-	//bfs(queue 사용)
-	B.push(start);   // 시작 vertex queue에 넣음
-	visited[start] = true;   // 시작 정점은 방문했으므로 true;
-	cout << start << " ";
-
-	while (!B.empty())
-	{
-		int current = B.front();
-		visited[current] = true;
-		B.pop();
-		for (int i = 0; i < 1000; i++)
-		{
-			if (graph[current][i] == 1 && !visited[i])
-			{
-				B.push(i);
-				visited[i] = true;
-				cout << i << " ";
-			}
-		}
+	
+	for (int i = 0; i < edge; i++) {   // 그래프 초기화
+		cin >> p1 >> p2;
+		graph[p1][p2] = 1;
+		graph[p2][p1] = 1;
 	}
 
+	/*for (int i = 0; i < graph.size(); i++) {
+		for (int j = 0; j < graph[i].size(); j++) {
+			cout << graph[i][j] << " ";
+		}
+		cout << endl;
+	}*/
+
+	dfs(start);   // vertex 0부터 탐색 시작
+	visited.assign(1001, false);   // bfs 위해 visited 초기화
+	cout << endl;
+	bfs(start);
 	return 0;
 }
 
